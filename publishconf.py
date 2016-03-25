e5d39e1aa5f3d0ca7a59ce7b9b0c5de554fde3a5
Achilleas Pipinellis <axilleas@axilleas.me>
2016-03-25 12:13:13 +0000

    Revert "Output dir is imported from pelicanconf.py"
    This reverts commit 29124a111b0c8b00d10f57a2c6e54e68759b155f

--- publishconf.py
+++ publishconf.py
@@ -10,13 +10,14 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://pages.gitlab.io/pelican'
+SITEURL = 'https://pages.gitlab.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_PATH = 'public'
 
 # Following items are often useful when publishing
 
